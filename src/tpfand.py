d1176d1ffdd63518cce86b41fc9952f7e5668143
Vladyslav Shtabovenko <dfew.entwickler@gmail.com>
2014-08-20 16:19:32 +0200

    Unite loose functions in tpfand.py to a new class Tpfand

--- src/tpfand.py
+++ src/tpfand.py
@@ -32,152 +32,156 @@ if not ('/usr/share/pyshared' in sys.path):
 if not ("/usr/lib/python2.7/site-packages" in sys.path):
     sys.path.append("/usr/lib/python2.7/site-packages")
 
-quiet = False
-debug = False
 
+class Tpfand(object):
 
-def daemon_main():
-    """daemon entry point"""
-    global controller, mainloop, act_settings
+    quiet = False
+    debug = False
 
-    # register SIGTERM handler
-    signal.signal(signal.SIGTERM, term_handler)
+    def __init__(self, quiet, debug):
+        self.quiet = quiet
+        self.debug = debug
 
-    # register d-bus service
-    dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
-    system_bus = dbus.SystemBus()
-    #name = dbus.service.BusName("org.thinkpad.fancontrol.tpfand", system_bus)
+        self.start_fan_control()
 
-    # create and load configuration
-    act_settings = settings.Settings(system_bus, '/Settings')
+    def start_fan_control(self):
+        """daemon start function"""
 
-    # create controller
-    controller = control.Control(system_bus, '/Control', act_settings, debug)
+        if not self.quiet:
+            print 'tpfand ' + build.version + ' - Copyright (C) 2011-2012 Vladyslav Shtabovenko'
+            print 'Copyright (C) 2007-2008 Sebastian Urban'
+            print 'This program comes with ABSOLUTELY NO WARRANTY'
+            print
+            print 'WARNING: THIS PROGRAM MAY DAMAGE YOUR COMPUTER.'
+            print '         PROCEED ONLY IF YOU KNOW HOW TO MONITOR SYSTEM TEMPERATURE.'
+            print
 
-    # start glib main loop
-    mainloop = gobject.MainLoop()
-    mainloop.run()
+        if self.debug:
+            print 'Running in debug mode'
 
+        if not self.is_system_suitable():
+            print "Fatal error: unable to set fanspeed, enable watchdog or read temperature"
+            print "             Please make sure you are root and a recent"
+            print "             thinkpad_acpi module is loaded with fan_control=1"
+            print "             If thinkpad_acpi is already loaded, check that"
+            print "             /proc/acpi/ibm/thermal exists. Thinkpad models"
+            print "             that doesn't have this file are currently unsupported"
+            exit(1)
 
-def term_handler(signum, frame):
-    """Handles SIGTERM"""
-    controller.set_speed(255)
-    try:
-        os.remove(build.pid_path)
-    except:
-        pass
-    mainloop.quit()
+        if os.path.isfile(build.pid_path):
+            print "Fatal error: already running or " + build.pid_path + " left behind"
+            exit(1)
 
+        # go into daemon mode
+        self.daemonize()
 
-def is_system_suitable():
-    """returns True iff fan speed setting, watchdog and thermal reading is supported by kernel and
-       we have write permissions"""
-    try:
-        fanfile = open(build.ibm_fan, 'w')
-        fanfile.write('level auto')
-        fanfile.flush()
-        fanfile.close()
-        fanfile = open(build.ibm_fan, 'w')
-        fanfile.write('watchdog 5')
-        fanfile.flush()
-        fanfile.close()
-
-        tempfile = open(build.ibm_thermal, 'r')
-        tempfile.readline()
-        tempfile.close()
-        return True
-    except IOError:
-        return False
-
-
-def start_fan_control(quiet):
-    """daemon start function"""
-
-    if not quiet:
-        print 'tpfand ' + build.version + ' - Copyright (C) 2011-2012 Vladyslav Shtabovenko'
-        print 'Copyright (C) 2007-2008 Sebastian Urban'
-        print 'This program comes with ABSOLUTELY NO WARRANTY'
-        print
-        print 'WARNING: THIS PROGRAM MAY DAMAGE YOUR COMPUTER.'
-        print '         PROCEED ONLY IF YOU KNOW HOW TO MONITOR SYSTEM TEMPERATURE.'
-        print
-
-    if debug:
-        print 'Running in debug mode'
-
-    if not is_system_suitable():
-        print "Fatal error: unable to set fanspeed, enable watchdog or read temperature"
-        print "             Please make sure you are root and a recent"
-        print "             thinkpad_acpi module is loaded with fan_control=1"
-        print "             If thinkpad_acpi is already loaded, check that"
-        print "             /proc/acpi/ibm/thermal exists. Thinkpad models"
-        print "             that doesn't have this file are currently unsupported"
-        exit(1)
-
-    if os.path.isfile(build.pid_path):
-        print "Fatal error: already running or " + build.pid_path + " left behind"
-        exit(1)
-
-    # go into daemon mode
-    daemonize()
-
-
-def daemonize():
-    """ don't go into daemon mode if debug mode is active """
-    if not debug:
-        """go into daemon mode"""
-        # from: http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/66012
-        # do the UNIX double-fork magic, see Stevens' "Advanced
-        # Programming in the UNIX Environment" for details (ISBN 0201563177)
+    def is_system_suitable(self):
+        """returns True iff fan speed setting, watchdog and thermal reading is supported by kernel and
+           we have write permissions"""
         try:
-            pid = os.fork()
-            if pid > 0:
-                # exit first parent
-                sys.exit(0)
-        except OSError, e:
-            print >>sys.stderr, "fork #1 failed: %d (%s)" % (
-                e.errno, e.strerror)
-            sys.exit(1)
+            fanfile = open(build.ibm_fan, 'w')
+            fanfile.write('level auto')
+            fanfile.flush()
+            fanfile.close()
+            fanfile = open(build.ibm_fan, 'w')
+            fanfile.write('watchdog 5')
+            fanfile.flush()
+            fanfile.close()
 
-        # decouple from parent environment
-        os.chdir("/")
-        os.setsid()
-        os.umask(0)
-
-        # do second fork
-        try:
-            pid = os.fork()
-            if pid > 0:
-                sys.exit(0)
-        except OSError, e:
-            print >>sys.stderr, "fork #2 failed: %d (%s)" % (
-                e.errno, e.strerror)
-            sys.exit(1)
-
-        # write pid file
-        try:
-            pidfile = open(build.pid_path, 'w')
-            pidfile.write(str(os.getpid()) + "\n")
-            pidfile.close()
+            tempfile = open(build.ibm_thermal, 'r')
+            tempfile.readline()
+            tempfile.close()
+            return True
         except IOError:
-            print >>sys.stderr, "could not write pid-file: ", build.pid_path
-            sys.exit(1)
+            return False
 
-    # start the daemon main loop
-    daemon_main()
+    def daemonize(self):
+        """ don't go into daemon mode if debug mode is active """
+        if not self.debug:
+            """go into daemon mode"""
+            # from: http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/66012
+            # do the UNIX double-fork magic, see Stevens' "Advanced
+            # Programming in the UNIX Environment" for details (ISBN
+            # 0201563177)
+            try:
+                pid = os.fork()
+                if pid > 0:
+                    # exit first parent
+                    sys.exit(0)
+            except OSError, e:
+                print >>sys.stderr, "fork #1 failed: %d (%s)" % (
+                    e.errno, e.strerror)
+                sys.exit(1)
+
+            # decouple from parent environment
+            os.chdir("/")
+            os.setsid()
+            os.umask(0)
+
+            # do second fork
+            try:
+                pid = os.fork()
+                if pid > 0:
+                    sys.exit(0)
+            except OSError, e:
+                print >>sys.stderr, "fork #2 failed: %d (%s)" % (
+                    e.errno, e.strerror)
+                sys.exit(1)
+
+            # write pid file
+            try:
+                pidfile = open(build.pid_path, 'w')
+                pidfile.write(str(os.getpid()) + "\n")
+                pidfile.close()
+            except IOError:
+                print >>sys.stderr, "could not write pid-file: ", build.pid_path
+                sys.exit(1)
+
+        # start the daemon main loop
+        self.daemon_main()
+
+    def daemon_main(self):
+        """daemon entry point"""
+
+        # register SIGTERM handler
+        signal.signal(signal.SIGTERM, self.term_handler)
+
+        # register d-bus service
+        dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+        system_bus = dbus.SystemBus()
+        #name = dbus.service.BusName("org.thinkpad.fancontrol.tpfand", system_bus)
+
+        # create and load configuration
+        act_settings = settings.Settings(system_bus, '/Settings')
+
+        # create controller
+        controller = control.Control(
+            system_bus, '/Control', act_settings, self.debug)
+
+        # start glib main loop
+        mainloop = gobject.MainLoop()
+        mainloop.run()
+
+    def term_handler(self, signum, frame):
+        """Handles SIGTERM"""
+        controller.set_speed(255)
+        try:
+            os.remove(build.pid_path)
+        except:
+            pass
+        mainloop.quit()
 
 
 def main():
-    global debug
-    global quiet
+    quiet = False
+    debug = False
 
     if "--quiet" in sys.argv:
         quiet = True
 
     if "--debug" in sys.argv:
         debug = True
-
-    start_fan_control(quiet)
+    app = Tpfand(quiet, debug)
 
 if __name__ == "__main__":
     main()
